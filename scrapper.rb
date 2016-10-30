82361e9cc8bb207ab9a172fb5a3a79fa7a2a6b86
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-10-30 15:45:26 -0400

    Gets script with picture urls and create an array with only the big ones

--- scrapper.rb
+++ scrapper.rb
@@ -3,6 +3,16 @@ require 'open-uri'
 
 url = "http://galleries.nascar.com/gallery/736/at-track-photos-sunday-martinsville"
 
-data = Nokogiri::HTML(open(url))
+raw_data = Nokogiri::HTML(open(url))
 
-puts data
+scripts_data = raw_data.css('script')[-2].text.split(',') #Second to last script item, which includes picture urls
+
+picture_data = []
+
+scripts_data.each do |item|
+	if item.include?('big_thumbnail') && item.include?('"big":')
+		picture_data.push(item)
+	end
+end
+
+puts picture_data
